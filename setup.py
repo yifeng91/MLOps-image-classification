211cd7ccad0ee327f32b3f52b5e3be9c0b28ad37
Yifeng <yifengtian91@gmail.com>
2024-04-10 16:52:26 -0600

    requirements added

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chest-Cancer-Classification-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "MLOps-image-classification"
+AUTHOR_USER_NAME = "yifeng91"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "yifengtian91@gmail.com"
 
 
 setuptools.setup(
